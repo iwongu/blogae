2e364375c09e14fa74e66a92bde2c19086424a25
Wongoo Lee <iwongu@gmail.com>
2014-04-11 17:49:42 -0700

    use controllerAs syntax.

--- js/admin.js
+++ js/admin.js
@@ -2,63 +2,65 @@
 
 
 // Declare app level module which depends on filters, and services
-var admin = angular.module('blogae.admin', ['blogae.topbar', 'blogae.scroll']);
+var adminApp = angular.module('blogae.admin', ['blogae.topbar', 'blogae.scroll']);
 
-admin.controller('AdminCtrl', function($scope, $http, $window, topbar) {
-  $scope.title = '';
-  $scope.content = '';
-  $scope.content_html = '';
-  $scope.tags = '';
-  $scope.permalink = '';
-  $scope.date_published = '';
+adminApp.controller('AdminCtrl', function($scope, $http, $window, topbar) {
+  var admin = this;
 
-  $scope.posts = [];
-  $scope.next_post_id = '';
+  admin.title = '';
+  admin.content = '';
+  admin.content_html = '';
+  admin.tags = '';
+  admin.permalink = '';
+  admin.date_published = '';
 
-  $scope.selected_post = null;
-  $scope.edited = false;
+  admin.posts = [];
+  admin.next_post_id = '';
 
-  $scope.help_showing = false;
+  admin.selected_post = null;
+  admin.edited = false;
 
-  $scope.saving = false;
-  $scope.metadata_editing = 'tags'; // or published or permalink.
-  $scope.blogae_scripts = $blogae_scripts;
+  admin.help_showing = false;
+
+  admin.saving = false;
+  admin.metadata_editing = 'tags'; // or published or permalink.
+  admin.blogae_scripts = $blogae_scripts;
 
 
-  $scope.edit = function(postid) {
-    angular.forEach($scope.posts, function(post) {
+  admin.edit = function(postid) {
+    angular.forEach(admin.posts, function(post) {
       if (post.id == postid) {
-        $scope.fill_post_form(post);
+        admin.fill_post_form(post);
       }
     });
   }
 
   // fills the post form. will lose the current post if any.
-  $scope.fill_post_form = function(post) {
-    if ($scope.has_changed() &&
+  admin.fill_post_form = function(post) {
+    if (admin.has_changed() &&
 	!$window.confirm('You will lose you data. Are you sure?')) {
       return;
     }
 
-    $scope.selected_post = post;
-    $scope.title = post.title;
-    $scope.content = post.content;
-    $scope.content_html = post.content_html;
-    $scope.tags = post.tags;
-    $scope.permalink = post.permalink;
-    $scope.date_published = post.date_published;
+    admin.selected_post = post;
+    admin.title = post.title;
+    admin.content = post.content;
+    admin.content_html = post.content_html;
+    admin.tags = post.tags;
+    admin.permalink = post.permalink;
+    admin.date_published = post.date_published;
 
-    $scope.edited = false;
-    $scope.content_changed();
+    admin.edited = false;
+    admin.content_changed();
   }
 
-  $scope.has_changed = function() {
-    var is_empty = !($scope.title || $scope.content || $scope.selected_post);
-    return !is_empty && $scope.edited;
+  admin.has_changed = function() {
+    var is_empty = !(admin.title || admin.content || admin.selected_post);
+    return !is_empty && admin.edited;
   }
 
-  $scope.view = function(postid) {
-    angular.forEach($scope.posts, function(post) {
+  admin.view = function(postid) {
+    angular.forEach(admin.posts, function(post) {
       if (post.id == postid) {
 	var permalink = post.permalink_full;
 	$window.open(permalink, 'view');
@@ -66,8 +68,8 @@ admin.controller('AdminCtrl', function($scope, $http, $window, topbar) {
     });
   }
 
-  $scope.content_changed = function() {
-    var html = markdown.toHTML($scope.content);
+  admin.content_changed = function() {
+    var html = markdown.toHTML(admin.content);
     if (/iframe.*youtube.com.*\/iframe/.exec(html)) {
       // post-process to support youtube iframes.
       html = html.replace(/&lt;iframe /g, '<iframe ');
@@ -80,37 +82,37 @@ admin.controller('AdminCtrl', function($scope, $http, $window, topbar) {
     $('#post-preview').html(html);
   }
 
-  $scope.toggle_metadata_editing = function() {
-    if ($scope.metadata_editing == 'tags') {
-      $scope.metadata_editing = 'published';
-    } else if ($scope.metadata_editing == 'published') {
-      $scope.metadata_editing = 'permalink';
+  admin.toggle_metadata_editing = function() {
+    if (admin.metadata_editing == 'tags') {
+      admin.metadata_editing = 'published';
+    } else if (admin.metadata_editing == 'published') {
+      admin.metadata_editing = 'permalink';
     } else {
-      $scope.metadata_editing = 'tags';
+      admin.metadata_editing = 'tags';
     }
   }
 
-  $scope.check_date_format = function() {
+  admin.check_date_format = function() {
     var format = /\d{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} \d{1,2}:\d{1,2}/;
-    return $scope.date_published.length == 0 || format.test($scope.date_published);
+    return admin.date_published.length == 0 || format.test(admin.date_published);
   }
 
-  $scope.delete_post = function() {
+  admin.delete_post = function() {
     if (!$window.confirm('Are you sure you delete the post?')) {
       return;
     }
 
-    var params = $.param({'postid': $scope.selected_post.id});
+    var params = $.param({'postid': admin.selected_post.id});
     topbar.show_message('deleting...');
     $http.post('/_/delete/', params).success(function(data) {
       var i = 0;
-      for (; i < $scope.posts.length; i++) {
-	if ($scope.posts[i].id == data.postid) {
+      for (; i < admin.posts.length; i++) {
+	if (admin.posts[i].id == data.postid) {
 	  break;
 	}
       }
-      $scope.posts.splice(i, 1);
-      $scope.prepare_new_post();
+      admin.posts.splice(i, 1);
+      admin.prepare_new_post();
       topbar.hide_message();
     }).error(function() {
       // todo(iwongu): show better error message.
@@ -118,109 +120,109 @@ admin.controller('AdminCtrl', function($scope, $http, $window, topbar) {
     });
   }
 
-  $scope.publish = function() {
-    $scope.save_post('/_/publish/', false);
+  admin.publish = function() {
+    admin.save_post('/_/publish/', false);
   }
 
-  $scope.save = function() {
-    $scope.save_post('/_/save/', true);
+  admin.save = function() {
+    admin.save_post('/_/save/', true);
   }
 
-  $scope.save_post = function(api_path, is_draft) {
-    if (!$scope.check_date_format()) {
+  admin.save_post = function(api_path, is_draft) {
+    if (!admin.check_date_format()) {
       $window.alert('The published date format is wrong.');
       return;
     }
-    $scope.saving = true;
-    var params = $scope.prepare_save();
+    admin.saving = true;
+    var params = admin.prepare_save();
     topbar.show_message('saving...');
     $http.post(api_path, params).success(function(post) {
-      $scope.saving = false;
-      $scope.edited = false;
-      if (!$scope.selected_post) { // new post.
-	$scope.selected_post = post;
-	$scope.posts.unshift(post);
+      admin.saving = false;
+      admin.edited = false;
+      if (!admin.selected_post) { // new post.
+	admin.selected_post = post;
+	admin.posts.unshift(post);
       } else {
-	angular.forEach($scope.posts, function(current, i) {
-	  if (current.id == $scope.selected_post.id) {
-	    $scope.posts[i] = post;
+	angular.forEach(admin.posts, function(current, i) {
+	  if (current.id == admin.selected_post.id) {
+	    admin.posts[i] = post;
 	  }
 	});
       }
-      $scope.fill_post_form(post);
+      admin.fill_post_form(post);
       topbar.hide_message();
     }).error(function() {
-      $scope.saving = false;
+      admin.saving = false;
       // todo(iwongu): show better error message.
       topbar.show_error('failed to save...');
     });
   }
 
-  $scope.new_post = function() {
-    if ($scope.has_changed() &&
+  admin.new_post = function() {
+    if (admin.has_changed() &&
 	!$window.confirm('You will lose you data. Are you sure?')) {
       return;
     }
-    $scope.prepare_new_post();
+    admin.prepare_new_post();
   }
 
-  $scope.prepare_new_post = function() {
-    $scope.title = '';
-    $scope.content = '';
-    $scope.content_html = '';
-    $scope.tags = '';
-    $scope.permalink = '';
-    $scope.date_published = '';
+  admin.prepare_new_post = function() {
+    admin.title = '';
+    admin.content = '';
+    admin.content_html = '';
+    admin.tags = '';
+    admin.permalink = '';
+    admin.date_published = '';
 
-    $scope.selected_post = null;
-    $scope.edited = false;
-    $scope.content_changed();
+    admin.selected_post = null;
+    admin.edited = false;
+    admin.content_changed();
   }
 
-  $scope.prepare_save = function() {
+  admin.prepare_save = function() {
     // save changes locally.
-    if ($scope.selected_post) {
-      $scope.selected_post.title = $scope.title;
-      $scope.selected_post.content = $scope.content;
-      $scope.selected_post.tags = $scope.tags;
-      $scope.selected_post.permalink = $scope.permalink;
-      $scope.selected_post.date_published = $scope.date_published;
+    if (admin.selected_post) {
+      admin.selected_post.title = admin.title;
+      admin.selected_post.content = admin.content;
+      admin.selected_post.tags = admin.tags;
+      admin.selected_post.permalink = admin.permalink;
+      admin.selected_post.date_published = admin.date_published;
     }
 
     return $.param({
-      'postid': $scope.selected_post ? $scope.selected_post.id : '',
-      'title': $scope.title,
-      'content': $scope.content,
-      'tags': $scope.tags,
-      'permalink': $scope.selected_post ? $scope.selected_post.permalink : '',
-      'date_published': $scope.selected_post ?
-        $scope.selected_post.date_published : '',
+      'postid': admin.selected_post ? admin.selected_post.id : '',
+      'title': admin.title,
+      'content': admin.content,
+      'tags': admin.tags,
+      'permalink': admin.selected_post ? admin.selected_post.permalink : '',
+      'date_published': admin.selected_post ?
+        admin.selected_post.date_published : '',
     });
   }
 
-  $scope.load_more = function() {
-    if ($scope.next_post_id) {
-      $scope.fetch_next($scope.next_post_id);
+  admin.load_more = function() {
+    if (admin.next_post_id) {
+      admin.fetch_next(admin.next_post_id);
     }
   };
 
-  $scope.call_blogae_script = function(num) {
-    $scope.content = $scope.blogae_scripts[num]($scope.content);
-    $scope.edited = true;
-    $scope.content_changed();
+  admin.call_blogae_script = function(num) {
+    admin.content = admin.blogae_scripts[num](admin.content);
+    admin.edited = true;
+    admin.content_changed();
   }
 
-  $scope.open_photo_picker = function() {
+  admin.open_photo_picker = function() {
     var picker = $window.open('/admin/picker', 'picker');
     var timer = $window.setInterval(function() {
       if (picker.closed !== false) {
 	$window.clearInterval(timer);
-	$scope.photos_selected();
+	admin.photos_selected();
       }
     }, 200);
   }
 
-  $scope.photos_selected = function() {
+  admin.photos_selected = function() {
     var value = $('#selected_photos').val();
     if (!value) {
       return;
@@ -228,26 +230,26 @@ admin.controller('AdminCtrl', function($scope, $http, $window, topbar) {
     var photos = value.split(' ');
     var content_el = $('#content').get(0);
     var caret = content_el.selectionStart;
-    var new_content = $scope.content.slice(0, caret);
+    var new_content = admin.content.slice(0, caret);
     new_content += '\n';
     for (var i = 0; i < photos.length; i++) {
       new_content += '\n![](' + photos[i] + ')\n';
     }
     new_content += '\n';
-    new_content += $scope.content.slice(caret, $scope.content.length);
-    $scope.content = new_content;
-    $scope.edited = true;
+    new_content += admin.content.slice(caret, admin.content.length);
+    admin.content = new_content;
+    admin.edited = true;
 
-    $scope.$apply();
-    $scope.content_changed();
+    admin.$apply();
+    admin.content_changed();
   }
 
-  $scope.fetch_next = function(next_post_id) {
+  admin.fetch_next = function(next_post_id) {
     var params = $.param({'next_post_id': next_post_id});
     topbar.show_message('loading...');
     $http.post('/_/get_posts/', params).success(function(data) {
-      $scope.posts = $scope.posts.concat(data.posts);
-      $scope.next_post_id = data.next_post_id;
+      admin.posts = admin.posts.concat(data.posts);
+      admin.next_post_id = data.next_post_id;
       topbar.hide_message();
     }).error(function() {
       // todo(iwongu): show beter error message.
@@ -260,5 +262,5 @@ admin.controller('AdminCtrl', function($scope, $http, $window, topbar) {
     "application/x-www-form-urlencoded";
 
   // fetch the posts when app is started.
-  $scope.fetch_next('')
+  admin.fetch_next('')
 });
